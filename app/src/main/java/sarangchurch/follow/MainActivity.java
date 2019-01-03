b9daff162af98841ddab267f73285a666a2dd9fd
jmkang12 <jmkang0811@naver.com>
2019-01-03 22:38:34 +0900

    Add 기능

--- app/src/main/java/sarangchurch/follow/MainActivity.java
+++ app/src/main/java/sarangchurch/follow/MainActivity.java
@@ -1,13 +1,32 @@
 package sarangchurch.follow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    Button buttonAddItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonAddItem = (Button)findViewById(R.id.btn_add_item);
+        buttonAddItem.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if(v==buttonAddItem){
+
+            Intent intent = new Intent(getApplicationContext(),Additem.class);
+            startActivity(intent);
+        }
+
     }
 }
